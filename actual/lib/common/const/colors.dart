1ce8d7eb71ce38d872c76ce10619d65995006f14
iyes826 <treeyu12345@naver.com>
2023-08-15 04:30:43 +0900

    asset 파일 추가

---
+++ actual/lib/common/const/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// 주색상
+const primaryColor = Color(0xFF22A45D);
+
+// 글자 색상
+const bodyTextColor = Color(0xFF868686);
+
+// 텍스트필드 배경 색상
+const inputBackgroundColor = Color(0xFFFBFBFB);
+// 텍스트필드 테두리 색상
+const inputBorderColor = Color(0xFFF3F2F2);
